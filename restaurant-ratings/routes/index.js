736af9ed04f4c44bfbfd00822df1428c39f3a2dd
James Aubrey <jaaubrey92@gmail.com>
2023-05-31 23:18:56 -0500

    home page style

--- restaurant-ratings/routes/index.js
+++ restaurant-ratings/routes/index.js
@@ -4,7 +4,7 @@ const passport = require('passport')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Restaurant Reviews' })
+  res.render('index', { title: 'Tulsa Restaurant Reviews' })
 })
 
 // Google OAuth login route
